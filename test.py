2a9349da24698d164a0ca32fe3b76d8144b89bcd
Riiyu99 <iuriborges32@hotmail.com>
2024-10-13 23:06:26 +0200

    Ajout d'une limite personnalisée `n` pour la boucle dans la méthode affiche

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class FizzBuzzs:
 
     def affiche(self, n):
         result=""
-        for i in range(1,100):
+        for i in range(1,n +1):
             if i % 15 == 0:
                 result+="FrisBee"
             elif i % 5 == 0:
