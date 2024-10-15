0b1e087e107dd280325279885b3584469917e332
Riiyu99 <iuriborges32@hotmail.com>
2024-10-15 22:18:50 +0200

    Création de la methode crypt

--- testcryp.py
+++ testcryp.py
@@ -4,6 +4,8 @@ class Cryptage:
     def __init__(self, nom):
         self.nom = nom
 
-    
+    def crypt(self, message):
+        caracteres = string.ascii_letters + string.digits + string.punctuation + " "
+        message_crypte = ""
         
         
