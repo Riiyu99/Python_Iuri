35795745e7c4d392dd0f1017e0637d4ea9d8d994
Riiyu99 <iuriborges32@hotmail.com>
2024-10-15 22:19:45 +0200

    Ajout de la boucle for

--- testcryp.py
+++ testcryp.py
@@ -8,4 +8,11 @@ class Cryptage:
         caracteres = string.ascii_letters + string.digits + string.punctuation + " "
         message_crypte = ""
         
+        for char in message:
+            if char in caracteres:
+                index = caracteres.index(char)
+                message_crypte += caracteres[(index + 1) % len(caracteres)]  
+            else:
+                message_crypte += char  
         
+        return message_crypte
