4bb1c536e2aca836fc62f91021455fe600784582
Riiyu99 <iuriborges32@hotmail.com>
2024-10-20 16:44:37 +0200

    Nommage de mon instance

--- test_cryptage.py
+++ test_cryptage.py
@@ -4,7 +4,7 @@ from testcryp import Cryptage
 class TestCryptage(unittest.TestCase):
 
     def setUp(self):
-        self.monInstance = Cryptage()  # Instance de votre classe Cryptage
+        self.monInstance = Cryptage("Mon instance")  
 
     def test_cryptage_simple(self):
         message_crypte = self.monInstance.crypt("abc", 1)
@@ -23,20 +23,20 @@ class TestCryptage(unittest.TestCase):
 
     def test_pas_invalide(self):
         with self.assertRaises(ValueError):
-            self.monInstance.crypt("test", 10)  # Teste un pas invalide
+            self.monInstance.crypt("test", 10) 
 
     def test_decryptage(self):
         decrypted_message = self.monInstance.decrypt("bcd1")
         print(f"{decrypted_message} = bcd1")
-        self.assertEqual(decrypted_message, "abc")  # Teste le décryptage
+        self.assertEqual(decrypted_message, "abc") 
         
         decrypted_message = self.monInstance.decrypt("ifmmp!xpsme1")
         print(f"{decrypted_message} = ifmmp!xpsme1")
-        self.assertEqual(decrypted_message, "hello world")  # Teste le décryptage
+        self.assertEqual(decrypted_message, "hello world")  
         
         decrypted_message = self.monInstance.decrypt("jgnnq!yqtnf2")
         print(f"{decrypted_message} = jgnnq!yqtnf2")
-        self.assertEqual(decrypted_message, "hello world")  # Teste le décryptage
+        self.assertEqual(decrypted_message, "hello world")  
 
 if __name__ == '__main__':
     unittest.main()
