2830e572a214ba2b63e57ab135a31f0af5b5ebca
Riiyu99 <iuriborges32@hotmail.com>
2024-10-20 14:43:28 +0200

    Ajout du cryptage avec pas et gestion des pas invalides, avec tests.

--- test_cryptage.py
+++ test_cryptage.py
@@ -1,5 +1,5 @@
 import unittest
-from  testcryp import Cryptage 
+from testcryp import Cryptage 
 
 class TestCryptage(unittest.TestCase):
     
@@ -8,17 +8,23 @@ class TestCryptage(unittest.TestCase):
         self.monInstance = Cryptage("MonInstance")
     
     def test_cryptage_simple(self):
-        self.assertEqual(self.monInstance.crypt("abc"), "bcd")
-        self.assertEqual(self.monInstance.crypt("ABC"), "BCD")
-        self.assertEqual(self.monInstance.crypt("123"), "234")
-        self.assertEqual(self.monInstance.crypt("! "), '"!')
-    
+        self.assertEqual(self.monInstance.crypt("abc", 1), "bcd1")
+        self.assertEqual(self.monInstance.crypt("ABC", 1), "BCD1")
+        self.assertEqual(self.monInstance.crypt("123", 1), "2341")
+        self.assertEqual(self.monInstance.crypt("! ", 1), '"!1')
+
     def test_cryptage_espace(self):
-        self.assertEqual(self.monInstance.crypt("hello world"), "ifmmp!xpsme")
-        
+        self.assertEqual(self.monInstance.crypt("hello world", 1), "ifmmp!xpsme1")
+
+    def test_cryptage_avec_pas(self):
+        self.assertEqual(self.monInstance.crypt("hello world", 2), "jgnnq!yqtnf2")
+
+    def test_pas_invalide(self):
+        with self.assertRaises(ValueError):
+            self.monInstance.crypt("test", 10)
+
     def tearDown(self):
         print("-----------------Fin du Test--------------")
-
 
 if __name__ == '__main__':
     unittest.main()
