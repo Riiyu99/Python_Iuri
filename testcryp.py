7b107bb44da3de3bf8c0d2859c7fd45bd59a17d4
Riiyu99 <iuriborges32@hotmail.com>
2024-10-20 16:27:43 +0200

    Ajout de la méthode decrypt pour déchiffrer les messages cryptés.

--- testcryp.py
+++ testcryp.py
@@ -24,3 +24,25 @@ class Cryptage:
                 message_crypte += char
         
         return message_crypte + str(pas)
+
+
+    def decrypt(self, message):
+        # Récupère le pas à partir de la fin du message
+        pas = int(message[-1])  # Récupère le dernier caractère comme pas
+        message = message[:-1]  # Enlève le pas du message
+
+        caracteres = string.ascii_letters + string.digits + string.punctuation
+        message_decrypte = ""
+
+        for char in message:
+            if char == "!":
+                message_decrypte += " "  # Remplace ! par espace
+            elif char in caracteres:
+                index = caracteres.index(char)
+                # Décalage inverse du caractère
+                prev_char = caracteres[(index - pas) % len(caracteres)]
+                message_decrypte += prev_char
+            else:
+                message_decrypte += char  # Caractères non définis restent inchangés
+
+        return message_decrypte  # Renvoie le message déchiffré
